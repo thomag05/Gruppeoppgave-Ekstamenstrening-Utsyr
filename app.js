48c6fca77fd96744aabe864db3e33de36a3784ff
thomag05 <thomas.magnussen05@gmail.com>
2023-04-11 08:56:43 +0200

    First data

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const session = require("express-session");
+const bcrypt = require("bcrypt");
+const path = require("path");
+const hbs = require("hbs");
+
+const app = express();
+const rootpath = path.join(__dirname, "public")
+const hbspath = path.join(__dirname, "views")
+
+
+
+app.listen("3000", () => {
+    console.log("Server listening at http://localhost:3000")
+})
