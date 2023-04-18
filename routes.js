9028d55c126fbd3f0c0ed86ad9e750f1bee16b8e
SteinarTS01 <90866435+SteinarTS01@users.noreply.github.com>
2023-04-18 09:04:36 +0200

    lagt til login rute

--- routes.js
+++ routes.js
@@ -5,4 +5,22 @@ module.exports = function(app){
     //     res.render("index.html")
     // })
 
+    
+app.post("/login", async (req, res) => {
+    try {
+      let login = req.body;
+      let userData = db.prepare("SELECT * FROM brukere WHERE email = ?").get(login.email);
+      if(await bcrypt.compare(login.password, userData.hash)) {
+        req.session.loggedin = true
+        req.session.brukerid = userData.id
+        if(userData.admin === 1 ) {req.session.isAdmin = true}
+        res.redirect("/")
+      } else {
+        res.redirect("back")
+      }
+    } catch (err) {
+        res.send('<html><body><script>alert("Du har tastet inn feil brukernavn eller passord");window.location.href="/";</script></body></html>');
+    }
+  });
+
 }
